c7b2759e672a2ba08276e98a26f4483ad3950768
sanketudaypatil <sanketpatil@nimapinfotech.com>
2023-07-12 19:12:25 +0530

    added to cart functionality

--- src/bottom/Cart.js
+++ src/bottom/Cart.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import React, {useState} from 'react';
 import {FlatList} from 'react-native-gesture-handler';
 import CartItem from '../components/CartItem';
@@ -11,9 +11,9 @@ const Cart = () => {
   const dispatch = useDispatch();
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{ backgroundColor:"white"}}>
       <Headerss />
-      <FlatList
+      {cartData.length  > 0?(<FlatList
         data={cartData}
         renderItem={({item, index}) => {
           return (
@@ -29,7 +29,25 @@ const Cart = () => {
             />
           );
         }}
-      />
+      />):(
+        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+          <Image source={require("../Images/cart.png")} style={{width:170, height:170}}/>
+          <Text style={{color:"black", padding:15, fontWeight:"bold"}}>No Items Added In Cart</Text>
+        </View>
+      )}
+      {cartData.length  > 0 && (
+        <TouchableOpacity
+        style={{
+          backgroundColor: '#f0c14b',
+          borderWidth: 0,
+          padding: 10,
+          borderRadius: 10,
+        }}
+        >
+        <Text style={{color: 'white',}}>Remove Itemsss</Text>
+      </TouchableOpacity>
+      )}
+
     </View>
   );
 };
